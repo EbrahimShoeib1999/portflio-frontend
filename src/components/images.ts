5d1025e7eab3462eaeae306ba227926185f3f8d3
Ibrahim Shoeib <ibrahimshoeib255@gmail.com>
2026-07-20 19:43:41 +0300

    fix : build

--- src/components/images.ts
+++ src/components/images.ts
@@ -1,13 +1,13 @@
-import saifi from "../assets/projects Screen/Stable/dashBoard.png";
-import ecommerce from "../assets/projects Screen/ecommerce/26.10.2025_17.56.31_REC.png";
-import codex from "../assets/projects Screen/codex/Capture.png";
-import faria from "../assets/projects Screen/faria/etherum.png";
-import wadi from "../assets/projects Screen/wadi/Capture.jpeg";
-import workspace from "../assets/projects Screen/portflio/Capture.png";
-import adwallpro from "../assets/projects Screen/AdwallPro/AdWallPro-منصة-الإعلانات-العصري-12-30-2025_03_48_PM.png";
-import egyptra from "../assets/projects Screen/egyptra/WhatsApp Image 2026-07-20 at 4.13.36 PM.jpeg";
-import restaurant from "../assets/projects Screen/restaurant/Capture1.png";
-import deliveria from "../assets/projects Screen/delveria/WhatsApp Image 2026-07-20 at 5.33.24 PM.jpeg"
+import saifi from "../assets/projects-screen/Stable/dashBoard.png";
+import ecommerce from "../assets/projects-screen/ecommerce/26.10.2025_17.56.31_REC.png";
+import codex from "../assets/projects-screen/codex/Capture.png";
+import faria from "../assets/projects-screen/faria/etherum.png";
+import wadi from "../assets/projects-screen/wadi/Capture.jpeg";
+import workspace from "../assets/projects-screen/portflio/Capture.png";
+import adwallpro from "../assets/projects-screen/AdwallPro/AdWallPro-منصة-الإعلانات-العصري-12-30-2025_03_48_PM.png";
+import egyptra from "../assets/projects-screen/egyptra/WhatsApp Image 2026-07-20 at 4.13.36 PM.jpeg";
+import restaurant from "../assets/projects-screen/restaurant/Capture1.png";
+import deliveria from "../assets/projects-screen/delveria/WhatsApp Image 2026-07-20 at 5.33.24 PM.jpeg"
 export const images = {
   saifi,
   stable: saifi,
