7f25d33c70bbfe1cb089fe8affef92926fe94a33
Ibrahim Shoeib <ibrahimshoeib255@gmail.com>
2026-07-20 20:01:50 +0300

    delveria

--- src/components/images.ts
+++ src/components/images.ts
@@ -7,7 +7,7 @@ import workspace from "../assets/projects-screen/portflio/Capture.png";
 import adwallpro from "../assets/projects-screen/AdwallPro/AdWallPro-منصة-الإعلانات-العصري-12-30-2025_03_48_PM.png";
 import egyptra from "../assets/projects-screen/egyptra/WhatsApp Image 2026-07-20 at 4.13.36 PM.jpeg";
 import restaurant from "../assets/projects-screen/restaurant/Capture1.png";
-import deliveria from "../assets/projects-screen/delveria/WhatsApp Image 2026-07-20 at 5.33.24 PM.jpeg"
+import deliveria from "../assets/projects-screen/deliveria/Capture.png";
 export const images = {
   saifi,
   stable: saifi,
